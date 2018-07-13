646c79cfd446537580c7c2ed16dfd6bf0044cc7a
Luis David Goyes <lgoyes@condorlabs.io>
2018-07-13 11:04:03 -0500

    Organize initial directories

--- app/src/main/java/io/condorlabs/lgoyes/demomvp/login/LoginActivity.kt
+++ app/src/main/java/io/condorlabs/lgoyes/demomvp/login/LoginActivity.kt
@@ -1,12 +1,13 @@
-package io.condorlabs.lgoyes.demomvp
+package io.condorlabs.lgoyes.demomvp.login
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import io.condorlabs.lgoyes.demomvp.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
